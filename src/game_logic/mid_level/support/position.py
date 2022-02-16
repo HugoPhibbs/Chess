12427bdcfe68dd929fc9e8f23760d68d9c42288c
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-02-16 21:32:52 +1300

    refactored code

--- src/game_logic/mid_level/support/position.py
+++ src/game_logic/mid_level/support/position.py
@@ -1,4 +1,11 @@
-from classes.pieces.piece import Piece
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from src.game_logic.pieces.piece import Piece
+
+from src.game_logic.helpers import coords_helper
 
 
 class Position:
@@ -6,15 +13,18 @@ class Position:
     Represents a Position on a chessboard
     """
 
-    def __init__(self, coords, piece: Piece = None):
+    __piece: 'Piece' = None
+
+    def __init__(self, coords: tuple[int, int], piece: 'Piece' = None):
         """
         Constructor for a Position object
 
         :param coords: tuple for the coordinates of this position on a chess board. Expressed as a number pair, each
         number from 0-7, useful for indexing the board matrix
-        :param piece Piece object that this position contains, leave empty if no piece present
+        :param coords_helper: CoordsHelper object to help with dealing with coordinates
+        :param piece: Piece object that this position contains, leave empty if no piece present
         """
-        self.coords = coords
+        self.__set_coords(coords)
         self.piece = piece
 
     def __repr__(self) -> str:
@@ -23,34 +33,9 @@ class Position:
 
         :return: string as described
         """
-        return self.col_str() + self.row_str()
+        return self.__col_str() + self.__row_str()
 
-    @staticmethod
-    def coords_is_valid(val):
-        """
-        Finds if an inputted value is valid to be integer coordinate pair for this position
-
-        Must be a tuple of length 2, contain only integers, and both integers must be between 0 and 7 (inclusive)
-        :param val:
-        :return: bool if the inputted value is a valid coordinate pair or not
-        """
-        if not isinstance(val, tuple) or len(val) != 2:
-            return False
-        if not isinstance(val[0], int) or not isinstance(val[1], int):
-            return False
-        return Position.coords_in_board(val)
-
-    @staticmethod
-    def coords_in_board(coords) -> bool:
-        """
-        Finds out if a integer coordinate pair are on a chess board or not
-
-        :param coords: tuple of length 2 with integer values to be checked
-        :return: bool if the inputted coords are in the board or not
-        """
-        return 0 <= coords[0] <= 7 and 0 <= coords[1] <= 8
-
-    def col_str(self) -> str:
+    def __col_str(self) -> str:
         """
         Returns a string representation of the col of this position, as in an actual game of chess
 
@@ -60,7 +45,7 @@ class Position:
         """
         return chr(self.col + 65)
 
-    def row_str(self) -> str:
+    def __row_str(self) -> str:
         """
         Returns a string representation of this row of this position, as in an actual game of chess
 
@@ -71,31 +56,60 @@ class Position:
         return str(self.row + 1)
 
     @property
-    def piece(self) -> Piece:
+    def piece(self) -> 'Piece' | None:
+        """
+        The piece that currently occupies this position, None if there is nothing there
+
+        :return: Piece object or None as described
+        """
         return self.__piece
 
     @piece.setter
-    def piece(self, val) -> None:
-        assert isinstance(val, val)
+    def piece(self, val: 'Piece') -> None:
+        """
+        Sets which piece currently occupies this position
+
+        :param val: Piece object to be set
+        :return: None
+        """
         self.__piece = val
 
     @property
-    def coords(self):
+    def coords(self) -> tuple[int, int]:
+        """
+        The coordinates that this position occupies
+
+        :return: tuple of integers for the coordinates of this position
+        """
         return self.__coords
 
-    @coords.setter
-    def coords(self, val):
-        if Position.coords_is_valid(val):
-            self.__coords = val
-        else:
-            raise Exception("Inputted value for new coords for this position are not valid!")
+    def __set_coords(self, val: tuple[int, int]):
+        """
+        Checks then sets the coordinates of this position
+
+        :param val: tuple of integers to be checked if valid, then set as the coordinates of this position
+        :return: None
+        """
+        assert coords_helper.coords_are_valid(val), "Inputted value for new coords for this position are not valid!, " \
+                                                    "see Board.coords_are_valid(coords)"
+        self.__coords = val
 
     @property
     def row(self) -> int:
+        """
+        The row that this Position is on
+
+        :return: int as described
+        """
         return self.coords[0]
 
     @property
     def col(self) -> int:
+        """
+        The col that this Position is on
+
+        :return: int as described
+        """
         return self.coords[1]
 
     @property
@@ -105,9 +119,9 @@ class Position:
 
         :return: bool as described
         """
-        return self.piece is not None
+        return self.piece is None
 
-    def is_hostile(self, colour : str):
+    def is_hostile(self, colour: str) -> bool:
         """
         Finds out if this position is occupied by a piece that has a colour that is opposite to the inputted piece
 
@@ -120,7 +134,7 @@ class Position:
             return False
         return self.piece.colour != colour
 
-    def is_friendly(self, colour):
+    def is_friendly(self, colour: str) -> bool:
         """
         Finds out if this position is occupied by a piece that is considered friendly to another piece
 
@@ -147,15 +161,3 @@ class Position:
         if self.is_vacant:
             return False
         return self.piece.type == piece_type
-
-    def replace_piece(self, new_piece:Piece = None) -> Piece:
-        """
-        Replaces the piece that is on this board with a new piece
-
-        :param new_piece Piece object that will be placed on this position, default None
-        :return: Piece that has been replaced, None if there was no piece already on this position
-        """
-        assert new_piece is None or isinstance(new_piece, Piece)
-        piece = self.piece
-        self.piece = new_piece
-        return piece
