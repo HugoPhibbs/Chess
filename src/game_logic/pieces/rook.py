ca237a80f1a7c72e35ddd340616ee9b5eb0ef1e9
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-02-16 21:26:44 +1300

    refactored move directions

--- src/game_logic/pieces/rook.py
+++ src/game_logic/pieces/rook.py
@@ -1,5 +1,8 @@
-from classes.pieces.piece import Piece
-from classes.pieces.rangedpiece import RangedPiece
+from src.game_logic.pieces.rangedpiece import RangedPiece
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    pass
 
 
 class Rook(RangedPiece):
@@ -7,20 +10,15 @@ class Rook(RangedPiece):
     Represents a rook piece in a game of chess
     """
 
-    def __init__(self, *args):
+    is_castleable : bool = False
+
+    def __init__(self, colour, value=0, king=None):
         """
         Constructor for a Rook piece, all constructor arguments are passed to Piece
 
         :param args: parameters for the parent constructor of a Piece object
         """
-        super().__init__(*args)
-
-    def move_directions(self) -> list:
-        """
-        See RangedPiece.move_directions()
-        :return:
-        """
-        return self.straight_directions
+        super().__init__(colour, value, king)
 
     @property
     def type(self):
@@ -37,4 +35,10 @@ class Rook(RangedPiece):
 
         :return: str as described
         """
+        return "Rook"
+
+    def str_abr(self) -> str:
         return "R"
+
+    def bearings(self) -> list[str]:
+        return self.bearing_helper.straight_bearings()
