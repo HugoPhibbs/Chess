4e7440c9b996c8132ada8932426ac24c234d627b
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-04-04 22:29:36 +1200

    Changes to Bishop

--- src/game_logic/pieces/bishop.py
+++ src/game_logic/pieces/bishop.py
@@ -1,16 +1,22 @@
 from src.game_logic.pieces.rangedpiece import RangedPiece
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from src.game_logic.mid_level.move_finders.movefinder import MoveFinder
+    from src.game_logic.helpers import BearingHelper
+
 class Bishop(RangedPiece):
     """
     Represents a bishop piece in a game of chess
     """
 
-    def __init__(self, colour, value=0, king=None):
+    def __init__(self, colour, move_finder : "MoveFinder", bearing_helper: 'BearingHelper', value=0, king=None):
         """
         Constructor for a Bishop piece, all constructor arguments are passed to Piece
 
         """
-        super().__init__(colour, value, king)
+        super().__init__(colour, move_finder, bearing_helper, value, king)
 
     @property
     def type(self):
