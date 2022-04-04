beec61f4b33ec10d7a641a5c16f2526d47cd90c9
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-04-04 22:30:54 +1200

    changed to king
    added docs

--- src/game_logic/pieces/king.py
+++ src/game_logic/pieces/king.py
@@ -2,18 +2,46 @@ from src.game_logic.pieces.limitedpiece import LimitedPiece
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from src.game_logic.mid_level.moves import Move
-    from src.game_logic.mid_level.support.board import Board
+    from src.game_logic.mid_level.move_finders.movefinder import MoveFinder
 
 class King(LimitedPiece):
+    """
+    Class to represent a King piece in a game of chess
+    """
 
-    def __init__(self, colour, move_finder : 'MoveFinder', value=0, king=None):
+    __king: 'King' = None
+
+    def __init__(self, colour : str, move_finder : 'MoveFinder', value=0, king=None):
         """
         Constructor for a King piece, all constructor arguments are passed to Piece
 
+        :param colour: string for the colour of this King
+        :param move_finder: MoveFinder object that finds moves for this King
+        :param value: int for the value of this king
+        :param king: King object that is the king of this king
         """
         super().__init__(colour, move_finder, value, king)
         self.king = self
+
+    @property
+    def king(self) -> 'King':
+        """
+        The King object that is king to this king
+
+        :return: 'King' object as described
+        """
+        return self.__king
+
+    @king.setter
+    def king(self, king) -> None:
+        """
+        Sets the king object for this King. This must be either None or this king itself
+
+        :param king: King object as described
+        :return: None
+        """
+        assert king is None or king == self, "King attribute for this king must be either None or this King itself!"
+        self.__king = king
 
     @property
     def type(self):
@@ -42,5 +70,6 @@ class King(LimitedPiece):
         :param board: Board object being used for a chess game
         :return: bool for if this King is in check or not
         """
+        ## TODO fix this bellow
         return self.in_knight_check(board) or self.in_check_all_directions(board)
 
